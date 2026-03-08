2ebc5531fe56579b02a307fc31663d4ff0ac22a0
Alvin <laykimsengapple@gmail.com>
2026-03-08 16:31:13 +0900

    track model.py back

---
+++ model.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+
+
+# coding all attension mechanism in the transformer architecture, 
+
+input = torch.randn(2,6,3)
+ # this input correspond to a str : your journey starts with one step.
+
+class SelfAttention(nn.Module):
+    def __init__(self, dim_in, dim_out):
+        super(SelfAttention, self).__init__()
+        self.query_weight = nn.Linear(dim_in, dim_out, bias=False) 
+        self.key_weight   = nn.Linear(dim_in, dim_out, bias=False)  
+        self.value_weight = nn.Linear(dim_in, dim_out, bias=False) 
+    
+    def forward(self, input):
+        query_vector = self.query_weight(input)
+        key_vector   = self.key_weight(input)
+        value_vector = self.value_weight(input)
+        att_w = query_vector @ key_vector.transpose(-2,-1)
+        att_w = torch.softmax( att_w/key_vector.shape[-1]**0.5, dim =-1)
+        context_vector = att_w @ value_vector
+
+        return context_vector
+
+# build a causal attention model class using dropout
+# dropout : randomly mask some of the attention weight to prevent overfitting and improve generalization.
+
+class CausalAttention(nn.Module):
+    def __init__(self, dim_in, dim_out, dropout = 0.1, context_length = 512):
+        super(CausalAttention, self).__init__()
+        self.query_weight = nn.Linear(dim_in, dim_out, bias = False)
+        self.query_key = nn.Linear(dim_in, dim_out, bias = False)
+        self.query_value = nn.Linear(dim_in, dim_out, bias = False)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal = 1).bool())
+
+    def forward(self, input):
+        batch, num_tokens, dim_in = input.shape
+        query_vector = self.query_weight(input)
+        key_vector   = self.query_key(input)
+        value_vector = self.query_value(input)
+
+        att_score = query_vector @ key_vector.transpose(-2,-1)
+        att_score = att_score.masked_fill(self.mask[:num_tokens, :num_tokens] , float('-inf'))
+        att_w = torch.softmax(att_score / key_vector.shape[-1]**0.5, dim=-1)
+        att_w = self.dropout(att_w)
+        context_vector = att_w @ value_vector
+
+        return context_vector
+
+
+torch.manual_seed(123)
+dim_in = input.shape[-1] 
+dim_out = 2
+dropout = 0
+context_length = input.shape[1]
+model_1 = SelfAttention(dim_in, dim_out)
+model_2 = CausalAttention(dim_in, dim_out, dropout = dropout, context_length = context_length)
+output_1 = model_1(input)
+output_2 = model_2(input)
+print(output_1)
+print(output_2)
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, dim_in, dim_out, num_heads, dropout = 0.1, context_length = 512):    
+        super(MultiHeadAttention).__init__()
+
+    
